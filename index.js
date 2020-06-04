21a031f95015e2a404b5e08e7112b25b51a075b6
Symion Edwards <symion.edwards@gmail.com>
2020-06-04 10:26:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,9 +86,7 @@ function createRock(x) {
     }
 
     window.requestAnimationFrame(moveRock)
-    ROCKS.push(rock);
-    return rock
-}
+  
     // implement me!
     // (use the comments below to guide you!)
     /**
